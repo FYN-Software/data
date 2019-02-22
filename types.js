4e4e421cba5b10702a169e884bd4f7e437b2db71
Chris Kruining <chris@kruining.eu>
2019-02-22 07:46:49 +0100

    also exported number and boolean

--- types.js
+++ types.js
@@ -1,9 +1,13 @@
 import String from './type/string.js';
 import List from './type/list.js';
 import Enum from './type/enum.js';
+import Boolean from './type/boolean.js';
+import Number from './type/number.js';
 
 export {
     String,
     List,
     Enum,
+    Boolean,
+    Number,
 }
