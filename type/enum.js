1d60157a06bb7404ce09102d4e3f7b50449aae58
Chris Kruining <chris@kruining.eu>
2019-02-22 01:10:09 +0100

    added and implemented an enum type

---
+++ type/enum.js
@@ -0,0 +1,67 @@
+import Type from './type.js';
+
+const properties = Symbol('properties');
+const keys = Symbol('keys');
+
+export default class Enum extends Type
+{
+    set(v)
+    {
+        if((v instanceof this.constructor) === false)
+        {
+            const k = Array.from(this.constructor[keys].values(), k => `'${k}'`);
+
+            throw new Error(`'${v}' is not a value of this Enum, expected one of [${k}]`);
+        }
+
+        return v;
+    }
+
+    static [Symbol.hasInstance](v)
+    {
+        return typeof v === 'symbol' && this[keys].has(v);
+    }
+
+    static define(p)
+    {
+        const symbols = new Map();
+        const c = class extends this
+        {
+            static get [properties]()
+            {
+                return p;
+            }
+
+            static get [keys]()
+            {
+                return symbols;
+            }
+        };
+
+        for(const k of Object.keys(p))
+        {
+            const s = Symbol(k);
+
+            symbols.set(s, k);
+
+            Object.defineProperty(c, k, { value: s });
+        }
+
+        for(const k of Object.keys(Object.values(p)[0]))
+        {
+            Object.defineProperty(c.prototype, k, {
+                get()
+                {
+                    return p[symbols.get(this.__value)][k];
+                },
+            });
+        }
+
+        return c;
+    }
+
+    static valueOf(k)
+    {
+        return this[properties][this[keys].get(k)];
+    }
+}
--- types.js
+++ types.js
@@ -1,7 +1,9 @@
 import String from './type/string.js';
 import List from './type/list.js';
+import Enum from './type/enum.js';
 
 export {
     String,
     List,
+    Enum,
 }
