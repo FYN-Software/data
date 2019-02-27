f981666e86e256724f6e07bb62138c3173721a8f
Chris Kruining <chris@kruining.eu>
2019-02-27 22:13:13 +0100

    implemented preliminary number solution

--- type/number.js
+++ type/number.js
@@ -1,6 +1,31 @@
 import Type from './type.js';
 
+const min = Symbol('min');
+const max = Symbol('max');
+
 export default class extends Type
 {
+    min(i)
+    {
+        if(Number.isInteger(i) === false || i < 0)
+        {
+            throw new Error(`Expected an unsigned integer, got '${i}'`);
+        }
 
+        this[min] = i;
+
+        return this;
+    }
+
+    max(i)
+    {
+        if(Number.isInteger(i) === false || i < 0)
+        {
+            throw new Error(`Expected an unsigned integer, got '${i}'`);
+        }
+
+        this[max] = i;
+
+        return this;
+    }
 }
