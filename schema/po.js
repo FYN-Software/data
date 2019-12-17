73a89289082ead80174c80513a600614d596f693
Chris Kruining <c.kruining@fyn.nl>
2019-12-17 18:06:31 +0100

    cleaned up PO file schema

--- schema/po.js
+++ schema/po.js
@@ -1,69 +1,85 @@
-export default class Po
+import Schema from './schema.js';
+
+export default class Po extends Schema
 {
-    interpretate(lines, limit = null)
+    #path;
+
+    constructor(path)
     {
-        let k = '';
-        let i  = 0;
-        let values = {};
+        super();
 
-        out:
-        for(let line of lines)
-        {
-            let match = line.match(Po.regex);
-
-            if(match === null)
-            {
-                continue;
-            }
-
-            switch (match[1])
-            {
-                case "msgid":
-                    k = match[2];
-                    values[k] = '';
-
-                    i++;
-
-                    if(i === limit)
-                    {
-                        break out;
-                    }
-
-                    break;
-
-                case "msgstr":
-                    values[k] += match[2];
-                    break;
-            }
-        }
-
-        return values;
+        this.#path = path;
     }
 
-    aggregate(args)
+    prepare(query)
     {
-        const langs = Object.keys(args);
-        const keys = Object.values(args).map(a => Object.keys(a)).reduce((t, a) => [ ...t, ...a ].unique(), []);
-
-        return keys.map(k => {
-            const obj = { key: k };
-
-            for(let lang of langs)
-            {
-                obj[lang] = args[lang][k];
-            }
-
-            return obj;
-        });
+        return `${this.#path}`;
     }
-
-    deAggregate(args)
-    {
-        return args;
-    }
-
-    static get regex()
-    {
-        return /^(msgid|msgstr)\s"(.+)"$/;
-    }
+    //
+    // interpretate(lines, limit = null)
+    // {
+    //     let k = '';
+    //     let i  = 0;
+    //     let values = {};
+    //
+    //     out:
+    //         for(let line of lines)
+    //         {
+    //             let match = line.match(Po.regex);
+    //
+    //             if(match === null)
+    //             {
+    //                 continue;
+    //             }
+    //
+    //             switch (match[1])
+    //             {
+    //                 case "msgid":
+    //                     k = match[2];
+    //                     values[k] = '';
+    //
+    //                     i++;
+    //
+    //                     if(i === limit)
+    //                     {
+    //                         break out;
+    //                     }
+    //
+    //                     break;
+    //
+    //                 case "msgstr":
+    //                     values[k] += match[2];
+    //                     break;
+    //             }
+    //         }
+    //
+    //     return values;
+    // }
+    //
+    // aggregate(args)
+    // {
+    //     const langs = Object.keys(args);
+    //     const keys = Object.values(args).map(a => Object.keys(a)).reduce((t, a) => [ ...t, ...a ].unique(), []);
+    //
+    //     return keys.map(k => {
+    //         const obj = { key: k };
+    //
+    //         for(let lang of langs)
+    //         {
+    //             obj[lang] = args[lang][k];
+    //         }
+    //
+    //         return obj;
+    //     });
+    // }
+    //
+    // deAggregate(args)
+    // {
+    //     return args;
+    // }
+    //
+    // static get regex()
+    // {
+    //     return /^(msgid|msgstr)\s"(.+)"$/;
+    // }
 }
